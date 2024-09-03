5b134fbe5e530b8feca94372e61387c9d4763e3a
Moyaki Vincent <moyakivincent@yahoo.com>
2024-09-04 00:01:08 +0100

    Configured CORS and frontend origins

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from income import models
 from income.routes import router as income_router
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(income_router)
+
+# Define a list of allowed origins
+origins = [
+    "http://localhost",
+    "http://localhost:3000"  # Adjust as needed for your frontend URL
+]
+
+# Configure CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)

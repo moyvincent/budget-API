3a067e1dd866f4c7577a7c2145eb5b8f4358aed2
Moyaki Vincent <moyakivincent@yahoo.com>
2024-09-03 05:50:35 +0100

    Updated Income routes and Models

--- income/routes.py
+++ income/routes.py
@@ -1,12 +1,36 @@
-from typing import Union
-from fastapi import APIRouter
-from income.schemas import Income
+from typing import List, Union
+from fastapi import APIRouter, Depends
+from database import SessionLocal
+from income import models
+from income.schemas import Income, IncomeCreate
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     tags=["Income"]
 )
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.post("/income/new")
-def create_new_income(income: Income):
-    return {"message": "Income created successfully"}
+def create_new_income(income: IncomeCreate, db: Session = Depends(get_db)):
+    db_income = models.Income(**income.model_dump())
+    db.add(db_income)
+    db.commit()
+    db.refresh(db_income)
+    return {"message": "Income created successfully"}, db_income
+
+@router.get("/income/{income_id}", response_model= Income)
+def get_income_by_id(income_id: int, db: Session = Depends(get_db)):
+    db_income = db.query(models.Income).filter(models.Income.id == income_id).first()
+    return db_income
+
+@router.get("/incomes/", response_model= List[Income])
+def get_all_income(db: Session = Depends(get_db)):
+    db_income = db.query(models.Income).all()
+    return db_income
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from income import models
 from income.routes import router as income_router
+from database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(income_router)
